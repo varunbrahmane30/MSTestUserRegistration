5d584cc70645e15ab45b157f8633b0d850496684
name <varunbrahmane30@gmail.com>
2021-07-27 23:25:32 +0530

    [varun] added: First name method

---
+++ RegisterUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day20_UserRegistrationProblem
+{
+    public class RegisterUser
+    {
+        public static String Regex_Name = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
+
+        public bool validateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, Regex_Name) == true)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
