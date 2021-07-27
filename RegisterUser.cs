d6a5fd6f81701aac22235b9a31bb22e393d9daab
name <varunbrahmane30@gmail.com>
2021-07-28 00:04:20 +0530

    [varun] added: program file for Mobile Number

--- RegisterUser.cs
+++ RegisterUser.cs
@@ -8,11 +8,11 @@ namespace Day20_UserRegistrationProblem
     public class RegisterUser
     {
 
-        public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+        public static string Regex_MobileNo = "(0|91)?[ ][6-9][0-9]{9}";
 
-        public static bool validateEmail(string email)
+        public static bool validateMobileNo(string mobileNo)
         {
-            if (Regex.IsMatch(email, Regex_Email) == true)
+            if (Regex.IsMatch(mobileNo, Regex_MobileNo) == true)
                 return true;
             else
                 return false;
