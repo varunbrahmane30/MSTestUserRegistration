923c9b6a87d8816f226a347dc6c77bc8c364c8a3
name <varunbrahmane30@gmail.com>
2021-07-27 23:58:48 +0530

    [varun] added: program file for Email

--- RegisterUser.cs
+++ RegisterUser.cs
@@ -7,12 +7,12 @@ namespace Day20_UserRegistrationProblem
 {
     public class RegisterUser
     {
-       
-        public static string Regex_lName = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
-       
-        public static bool validateLastName(string lastName)
+
+        public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+
+        public static bool validateEmail(string email)
         {
-            if (Regex.IsMatch(lastName, Regex_lName) == true)
+            if (Regex.IsMatch(email, Regex_Email) == true)
                 return true;
             else
                 return false;
