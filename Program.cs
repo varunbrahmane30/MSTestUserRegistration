ca3a9c7299d098392cf3e5bc8b7c90c1fc02f402
name <varunbrahmane30@gmail.com>
2021-07-27 23:18:28 +0530

    [varun] added: main program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day20_UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using MS Test");
+        }
+    }
+}
